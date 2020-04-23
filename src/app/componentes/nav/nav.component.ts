7638d302643dc37783c3b9ed20443099453963b7
fausto_montiel <fmontiel@ewaysa.com>
2020-04-23 16:46:16 -0300

    Subo fix estilos

--- src/app/componentes/nav/nav.component.ts
+++ src/app/componentes/nav/nav.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -7,7 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  constructor( 
+    private route: ActivatedRoute,
+    private router: Router) { 
+   this.verifUserLogin();
+   
+  }
+
+  userLogueado:boolean;
+
+  verifUserLogin(){
+    var logueado = sessionStorage.getItem('login');
+  
+      if(logueado){
+        this.userLogueado = true;
+      }else
+      {
+        this.userLogueado = false;
+      }
+  }
+
+  terminarSession(){
+    sessionStorage.removeItem('login');
+    this.router.navigate(['/Principal']);
+    window.location.reload();
+
+  }
+
 
   ngOnInit(): void {
   }
--- src/app/componentes/principal/principal.component.ts
+++ src/app/componentes/principal/principal.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-principal',
@@ -11,8 +11,54 @@ export class PrincipalComponent implements OnInit {
     isFirstOpen: true,
     isFirstDisabled: false
   };
-  constructor() {  }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router) {  }
 
+
+  verifLoginJuegos(){
+    var logueado = sessionStorage.getItem('login');
+
+    if(logueado){
+      this.router.navigate(['/Juegos']);
+    }else
+    {
+      this.router.navigate(['/Login']);
+    }
+  }
+
+  verifLoginListado(){
+      var logueado = sessionStorage.getItem('login');
+  
+      if(logueado){
+        this.router.navigate(['/Listado']);
+      }else
+      {
+        this.router.navigate(['/Login']);
+      }
+    }
+    verifLoginConfig(){
+      var logueado = sessionStorage.getItem('login');
+  
+      if(logueado){
+        this.router.navigate(['/Juegos']);
+      }else
+      {
+        this.router.navigate(['/Login']);
+      }
+    }
+
+    verifLoginJugadores(){
+      var logueado = sessionStorage.getItem('login');
+  
+      if(logueado){
+        this.router.navigate(['/Jugadores']);
+      }else
+      {
+        this.router.navigate(['/Login']);
+      }
+    }
+    
   ngOnInit() {
   }
 
